11e3db756ddba00933da2bc0284c77aa553a8c49
Alvaro <alvaro-davi1@hotmail.com>
2020-11-12 17:10:22 -0300

    preparing PRINCIPAL.c file

--- PRINCIPAL.c
+++ PRINCIPAL.c
@@ -2,9 +2,35 @@
 #include <stdlib.h>
 #include <math.h>
 
+// definicao de tipos complexos
+typedef struct {
+	int c1;
+	float c2;
+	char c3;
+} tExemplo;
+
+// prototipos de funcoes
+void imprimeZero();
+
+
+// ---------------------------------------------------------------------------------------------
+
+// funcao principal
 int main()
 {
-	//;
+	imprimeZero();
 
 	return 0;
 }
+
+// ---------------------------------------------------------------------------------------------
+
+
+// todas as funcoes
+void imprimeZero()
+{
+	printf("0");
+	return;
+}
+
+
