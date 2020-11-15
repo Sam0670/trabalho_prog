caffeafde72eac1ecaa68ba2ac0206b4faf8de7a
Alvaro <alvaro-davi1@hotmail.com>
2020-11-15 02:04:04 -0300

    Add news funcs, but need test  file reader func

--- PRINCIPAL.c
+++ PRINCIPAL.c
@@ -4,14 +4,44 @@
 #include <math.h>
 
 // definicao de tipos complexos
-typedef struct {
-	int c1;
-	float c2;
-	char c3;
-} tExemplo;
+typedef struct
+{
+	int dia;
+	int mes;
+	int ano;
+} tData;
+
+typedef struct
+{
+	tData DataCadastro;
+	tData DataObito;
+	char Classificacao;
+	char Municipio;
+	tData IdadeNaDataNotificacao;
+	char ComorbidadePulmao;
+	char ComorbidadeCardio;
+	char ComorbidadeRenal;
+	char ComorbidadeDiabetes;
+	char ComorbidadeTabagismo;
+	char ComorbidadeObesidade;
+	char FicouInternado;
+} tDadosPaciente;
 
 // prototipos de funcoes
-void imprimeZero();
+void pularPrimeiraLinha(FILE *arq);
+void verifValores(int num, FILE *arq);
+int lerDataCadastro(FILE *arq);
+int lerDataObito(FILE *arq);
+int lerClassificacao(FILE *arq);
+int lerMunicipio(FILE *arq);
+int lerIdadeNaDataNotificacao(FILE *arq);
+int lerComorbidadePulmao(FILE *arq);
+int lerComorbidadeCardio(FILE *arq);
+int lerComorbidadeRenal(FILE *arq);
+int lerComorbidadeDiabetes(FILE *arq);
+int lerComorbidadeTabagismo(FILE *arq);
+int lerComorbidadeObesidade(FILE *arq);
+int lerFicouInternado(FILE *arq);
 
 
 // ---------------------------------------------------------------------------------------------
@@ -20,7 +50,44 @@ void imprimeZero();
 // funcao principal
 int main()
 {
-	imprimeZero();
+	FILE *arq; // ponteiro de arquivo, armazena o endereco das posicoes do arquivo
+
+	arq = fopen("./data/covid19ES.csv", "r"); // abrir arquivo (endereco_arquivo, MODO_abertura-leitura), funcao passando por referência
+
+	if (arq == NULL) // caso o arquivo nao exista, a funcao retorna um ponteiro nulo (NULL)
+	{
+		printf("Erro na abertura: arquivo nao encontrado!\n");
+		exit(1); // força o encerramento do programa (POR CONVENÇÃO: retorna 0 caso tudo ocorra bem, retorna um número diferente de 0 caso ocorra um erro)
+	}
+	else
+	{
+		pularPrimeiraLinha(arq); // ignora os primeiros caracteres ate o \n, ou seja, ate o fim da primeira linha
+
+		while (! feof(arq)) // enquanto arquivo nao chega ao fim...
+		{
+			char charac = fgetc(arq); // ler caractere
+
+			int i = 0;
+
+			while (i < 12) // 12 e a quantidade de colunas/ de cada paciente, logo serao realizadas 12 coletas por linha
+			{
+				if (charac == ',') // contabiliza as virgulas e ajuda a prever o que deve ser registrado
+				{
+					i++;
+				}
+				if (charac == '\n')
+				{
+					break;	// !!!!!!!!!!!!!!!!!! TESTAR, VERIFICAR LEITURA E NECESSIDADE
+				}
+				else
+				{
+					verifValores(i, arq); // verifica valores separados por virgulas e escolhe a funcao baseado no numero de virgulas registradas
+				}
+			}
+		}
+	}
+
+	fclose(arq); // fechar arquivo e limpar o que foi armazenado no buffer
 
 	return 0;
 }
@@ -30,10 +97,90 @@ int main()
 
 
 // todas as funcoes
-void imprimeZero()
+void pularPrimeiraLinha(FILE *arq)
 {
-	printf("0");
+	int cont = 0;
+
+	while (fgetc(arq) != '\n')
+	{
+		// apenas para esquecer a primeira linha
+	}
+	cont = 1;
+
 	return;
 }
 
+void verifValores(int num, FILE *arq)
+{
+	switch (num)
+	{
+	case 0:
+		lerDataCadastro(arq);
+		break;
+	case 1:
+		/* code */
+		break;
+	case 2:
+		/* code */
+		break;
+	case 3:
+		/* code */
+		break;
+	case 4:
+		/* code */
+		break;
+	case 5:
+		/* code */
+		break;
+	case 6:
+		/* code */
+		break;
+	case 7:
+		/* code */
+		break;
+	case 8:
+		/* code */
+		break;
+	case 9:
+		/* code */
+		break;
+	case 10:
+		/* code */
+		break;
+	case 11:
+		/* code */
+		break;
+	case 12:
+		/* code */
+		break;
+	default:
+		break;
+	}
 
+	return;
+}
+/*
+lerDataObito(charac)
+lerClassificacao(charac)
+lerMunicipio(charac)
+lerIdadeNaDataNotificacao(charac)
+lerComorbidadePulmao(charac)
+lerComorbidadeCardio(charac)
+lerComorbidadeRenal(charac)
+lerComorbidadeDiabetes(charac)
+lerComorbidadeTabagismo(charac)
+lerComorbidadeObesidade(charac)
+lerFicouInternado(charac)
+*/
+
+lerDataCadastro(FILE *arq)
+{
+	tData Cadastro;
+	int ano, mes, dia;
+
+	fscanf(arq, "%d-%d-%d,", &ano, &mes, &dia); 	// !!!!!!!!!!!!!!!!!! TESTAR E VERIFICAR LEITURA
+
+	Cadastro.ano = ano;
+	Cadastro.mes = mes;
+	Cadastro.dia = dia;
+}
